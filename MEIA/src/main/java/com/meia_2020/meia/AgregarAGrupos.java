1f224e4dcea36c9ec932a1eeb6d59826af8e5c32
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-10-25 01:01:48 -0600

    Guardar grupo nuevo

--- MEIA/src/main/java/com/meia_2020/meia/AgregarAGrupos.java
+++ MEIA/src/main/java/com/meia_2020/meia/AgregarAGrupos.java
@@ -122,6 +122,7 @@ public class AgregarAGrupos extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         Amigos = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        bCrearGrupo2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -156,6 +157,13 @@ public class AgregarAGrupos extends javax.swing.JFrame {
 
         jLabel3.setText("Grupos");
 
+        bCrearGrupo2.setLabel("Nuevo grupo");
+        bCrearGrupo2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bCrearGrupo2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -172,12 +180,14 @@ public class AgregarAGrupos extends javax.swing.JFrame {
                             .addComponent(jScrollPane1)
                             .addComponent(GrupoSeleccionado, javax.swing.GroupLayout.DEFAULT_SIZE, 88, Short.MAX_VALUE))
                         .addGap(18, 18, 18)
-                        .addComponent(jButton1))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jButton1)
+                            .addComponent(bCrearGrupo2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(Amigos, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 99, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(79, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -190,7 +200,10 @@ public class AgregarAGrupos extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 244, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(bCrearGrupo2)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(AmigoSeleccionado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -246,6 +259,11 @@ public class AgregarAGrupos extends javax.swing.JFrame {
       
     }//GEN-LAST:event_jButton1ActionPerformed
 
+    private void bCrearGrupo2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bCrearGrupo2ActionPerformed
+        var NuevoGrupoFrame = new CrearGrupo();
+        NuevoGrupoFrame.setVisible(true);
+    }//GEN-LAST:event_bCrearGrupo2ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -287,6 +305,9 @@ public class AgregarAGrupos extends javax.swing.JFrame {
     private javax.swing.JTextField GrupoSeleccionado;
     private javax.swing.JList<String> ListaAmigos;
     private javax.swing.JList<String> ListaGrupos;
+    private javax.swing.JButton bCrearGrupo;
+    private javax.swing.JButton bCrearGrupo1;
+    private javax.swing.JButton bCrearGrupo2;
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
