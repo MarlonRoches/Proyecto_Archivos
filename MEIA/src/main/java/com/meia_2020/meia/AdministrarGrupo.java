1c14055e43a0311721b2b457ebb5c2c7e4f2b516
MarlonRoches <roches856@gmail.com>
2020-10-25 22:21:18 -0600

    Finalizada administracion de grupos

--- MEIA/src/main/java/com/meia_2020/meia/AdministrarGrupo.java
+++ MEIA/src/main/java/com/meia_2020/meia/AdministrarGrupo.java
@@ -255,6 +255,9 @@ public class AdministrarGrupo extends javax.swing.JFrame {
         {
            JOptionPane.showMessageDialog(null, "Elija 2 opciones validas.");
         }
+        this.setVisible(false);
+        var form = new AdministrarGrupo();
+form.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
