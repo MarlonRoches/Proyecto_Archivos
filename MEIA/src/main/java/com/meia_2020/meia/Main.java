74c6a34c5037aab8d84acee3e11f40fd37a98962
marcelaestradarl@gmail.com <marcelaestradarl@gmail.com>
2020-11-16 00:20:58 -0600

    Cambio en el login

--- MEIA/src/main/java/com/meia_2020/meia/Main.java
+++ MEIA/src/main/java/com/meia_2020/meia/Main.java
@@ -469,8 +469,8 @@ public class Main extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        var nuevoLogin = new LoginForm();
         this.setVisible(false);
+        var nuevoLogin = new LoginForm();
         nuevoLogin.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
